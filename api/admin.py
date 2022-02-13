6bc6eefae5747bf9ae4ee3875aff4d4f772c5d65
rfn900 <rfn900@gmail.com>
2022-02-13 23:23:02 +0100

    Add Stock, Portfolio and Trade to models

--- api/admin.py
+++ api/admin.py
@@ -1,2 +1,7 @@
 from django.contrib import admin
+from .models import Stock, Portfolio, Trade
+
 # Register your models here.
+admin.site.register(Stock)
+admin.site.register(Portfolio)
+admin.site.register(Trade)
