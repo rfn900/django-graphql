86c3721657574630ca5d925149fcac796747e8f3
rfn900 <rfn900@gmail.com>
2022-02-13 23:21:44 +0100

    Add custom command to load json to db

---
+++ api/management/commands/loaddata.py
@@ -0,0 +1,18 @@
+from django.core.management.base import BaseCommand
+from api.models import Stock
+import json
+
+class Command(BaseCommand):
+    help = 'import stocks json file'
+
+    def add_arguments(self, parser):
+        pass
+
+    
+    def handle(self, *args, **options):
+        with open('instruments_data.json') as f:
+            data = json.load(f)
+            for stock in data:
+                models = Stock(name=stock['name'], symbol=stock['symbol'])
+                models.save()
+        
